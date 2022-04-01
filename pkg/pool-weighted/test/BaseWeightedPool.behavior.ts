e2f9cef2558e7bc92f53ca81f2bf4feb7a65c264
kryptachio <kryptachio@gmail.com>
2022-04-01 11:16:57 +0100

    Add decimals for DAI token in sample test

--- pkg/pool-weighted/test/BaseWeightedPool.behavior.ts
+++ pkg/pool-weighted/test/BaseWeightedPool.behavior.ts
@@ -50,7 +50,7 @@ export function itBehavesAsWeightedPool(
         },
         {
           symbol: 'DAI',
-          decimals: 0,
+          decimals: 18,
         },
       ],
       { sorted: true }
